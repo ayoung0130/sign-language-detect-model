2e4a65a099987442a5e4563526e87c446506cf99
ayoung0130 <149597700+ayoung0130@users.noreply.github.com>
2024-05-03 12:15:21 +0900

    0503_1

--- resize_dataset.py
+++ resize_dataset.py
@@ -1,27 +1,22 @@
 import cv2, os
 
-name = "토하다"
+name = ""
 
 # 파일 경로명 주의
-video_files = [f"C:/Users/_/Desktop/video/ID_1/{name}(정).avi", f"C:/Users/_/Desktop/video/ID_1/{name}(측).avi",
-               f"C:/Users/_/Desktop/video/ID_2/{name}(정).avi", f"C:/Users/_/Desktop/video/ID_2/{name}(측).avi",
-               f"C:/Users/_/Desktop/video/ID_3/{name}(정).avi", f"C:/Users/_/Desktop/video/ID_3/{name}(측).avi",
-               f"C:/Users/_/Desktop/video/ID_4/{name}(정).avi", f"C:/Users/_/Desktop/video/ID_4/{name}(측).avi",
-               f"C:/Users/_/Desktop/video/ID_5/{name}(정).avi", f"C:/Users/_/Desktop/video/ID_5/{name}(측).avi",
-               f"C:/Users/_/Desktop/video/ID_6/{name}(정).avi", f"C:/Users/_/Desktop/video/ID_6/{name}(측).avi",
-               f"C:/Users/_/Desktop/video/ID_7/{name}(정).avi", f"C:/Users/_/Desktop/video/ID_7/{name}(측).avi",
-               f"C:/Users/_/Desktop/video/ID_8/{name}(정).avi", f"C:/Users/_/Desktop/video/ID_8/{name}(측).avi",
-               f"C:/Users/_/Desktop/video/ID_9/{name}(정).avi", f"C:/Users/_/Desktop/video/ID_9/{name}(측).avi",
-               f"C:/Users/_/Desktop/video/ID_10/{name}(정).avi", f"C:/Users/_/Desktop/video/ID_10/{name}(측).avi"]
+video_files = [f"C:/Users/mshof/Desktop/video/test_video/가렵다.mp4", f"C:/Users/mshof/Desktop/video/test_video/기절.mp4",
+               f"C:/Users/mshof/Desktop/video/test_video/부러지다.mp4", f"C:/Users/mshof/Desktop/video/test_video/어제.mp4",
+               f"C:/Users/mshof/Desktop/video/test_video/어지러움.mp4", f"C:/Users/mshof/Desktop/video/test_video/열나다.mp4",
+               f"C:/Users/mshof/Desktop/video/test_video/오늘.mp4", f"C:/Users/mshof/Desktop/video/test_video/진통제.mp4",
+               f"C:/Users/mshof/Desktop/video/test_video/창백하다.mp4", f"C:/Users/mshof/Desktop/video/test_video/토하다.mp4"]
 
 # 인덱스 0(가렵다), 1(기절), 2(부러지다), 3(어제), 4(어지러움), 5(열나다), 6(오늘), 7(진통제), 8(창백하다), 9(토하다)
-output_folder = "C:/Users/_/Desktop/video/resized_video_9"
+output_folder = "C:/Users/mshof/Desktop/video/test_resize"
 os.makedirs(output_folder, exist_ok=True)
 
 # 너비, 높이 설정
 # 원본 영상과 비율은 동일하게 유지. 1280x720 (1.78 : 1)
-new_width = 712
-new_height = 400
+new_width = 400
+new_height = 300
 
 # 픽셀 크기 설정
 distance_from_center = 300
@@ -39,7 +34,7 @@ for video_file in video_files:
 
     output_path = os.path.join(output_folder, os.path.basename(f"{idx}_{os.path.basename(video_file)}"))
     fourcc = cv2.VideoWriter_fourcc(*'XVID')    # .avi 확장자로 설정
-    out = cv2.VideoWriter(output_path, fourcc, 30.0, (right_x - left_x, new_height))   # 30.0은 프레임
+    out = cv2.VideoWriter(output_path, fourcc, 30.0, (new_width, new_height))   # 30.0은 프레임
 
     while cap.isOpened():
         ret, frame = cap.read()
@@ -50,13 +45,13 @@ for video_file in video_files:
         resized_frame = cv2.resize(frame, (new_width, new_height))
 
         # 픽셀 크기 조절
-        cropped_frame = resized_frame[:, left_x:right_x]
+        # cropped_frame = resized_frame[:, left_x:right_x]
             
         # 조절된 프레임을 새로운 동영상에 추가
-        out.write(cropped_frame)
+        out.write(resized_frame)
 
         # 영상을 화면에 표시
-        cv2.imshow('MediaPipe', cropped_frame)
+        cv2.imshow('MediaPipe', resized_frame)
         if cv2.waitKey(1) == ord('q'):
             break
 
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import cv2
+import cv2, os, random
 import mediapipe as mp
 import numpy as np
 from setting import setVisibility
@@ -8,7 +8,7 @@ from PIL import ImageFont, ImageDraw, Image
 # 인덱스 0(가렵다), 1(기절), 2(부러지다), 3(어제), 4(어지러움), 5(열나다), 6(오늘), 7(진통제), 8(창백하다), 9(토하다)
 actions = ['가렵다', '기절', '부러지다', '어제', '어지러움', '열나다', '오늘', '진통제', '창백하다', '토하다']
 seq_length = 30
-font = ImageFont.truetype('fonts/MaruBuri-Bold.ttf', 30)
+font = ImageFont.truetype('fonts/NanumGothicBold.ttf', 20)
 
 # 모델 불러오기
 model = load_model('models/model.h5')
@@ -24,88 +24,95 @@ pose_landmark_indices = [0, 2, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 1
 mp_drawing = mp.solutions.drawing_utils
 
 # 웹캠 또는 비디오 파일 설정
-video_source = 0  # 웹캠 사용시 0, 비디오 파일 사용시 경로명 입력
-cap = cv2.VideoCapture(video_source)
+# video_source = 0  # 웹캠 사용시 0, 비디오 파일 사용시 경로명 입력
+# cap = cv2.VideoCapture(video_source)
+video_source = f"C:/Users/mshof/Desktop/video/test_resize"
 
-seq = []
-action_seq = []
+# 동영상 파일 목록을 랜덤으로 섞음
+video_files = os.listdir(video_source)
+print(video_files)
+random.shuffle(video_files)
+print(video_files)
 
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret:
-        break
+for video_file in video_files:
+    # 동영상 불러오기
+    video_path = os.path.join(video_source, video_file)
+    cap = cv2.VideoCapture(video_path)
 
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    results_hands = hands.process(frame)    # 손 랜드마크 검출
-    results_pose = pose.process(frame)      # 포즈 랜드마크 검출
-    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    data = []
 
-    # 관절 정보 저장할 넘파이 배열 초기화
-    joint_left_hands = np.zeros((21, 4))
-    joint_right_hands = np.zeros((21, 4))
-    joint = np.zeros((21, 12))
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results_hands = hands.process(frame)    # 손 랜드마크 검출
+        results_pose = pose.process(frame)      # 포즈 랜드마크 검출
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 
-    # 손 검출시
-    if results_hands.multi_hand_landmarks is not None:
-        for res, handedness in zip(results_hands.multi_hand_landmarks, results_hands.multi_handedness):
-                
-            # 손 -> 모든 관절에 대해 반복. 한 프레임에 왼손, 오른손 데이터가 0번부터 20번까지 들어감
-            for j, lm in enumerate(res.landmark):
+        # 관절 정보 저장할 넘파이 배열 초기화
+        joint_left_hands = np.zeros((21, 4))
+        joint_right_hands = np.zeros((21, 4))
+        joint = np.zeros((21, 12))
+
+        # 손 검출시
+        if results_hands.multi_hand_landmarks is not None:
+            for res, handedness in zip(results_hands.multi_hand_landmarks, results_hands.multi_handedness):    
+                # 손 -> 모든 관절에 대해 반복. 한 프레임에 왼손, 오른손 데이터가 0번부터 20번까지 들어감
+                for j, lm in enumerate(res.landmark):
+                    if handedness.classification[0].label == 'Left':
+                        joint_left_hands[j] = [lm.x, lm.y, lm.z, setVisibility(lm.x, lm.y)]
+                    else:
+                        joint_right_hands[j] = [lm.x, lm.y, lm.z, setVisibility(lm.x, lm.y)]
+                # 손 랜드마크 그리기
                 if handedness.classification[0].label == 'Left':
-                    joint_left_hands[j] = [lm.x, lm.y, lm.z, setVisibility(lm.x, lm.y)]
+                    mp_drawing.draw_landmarks(frame, res, mp_hands.HAND_CONNECTIONS, landmark_drawing_spec=mp_drawing.DrawingSpec(color=(0, 255, 0)))   # green
                 else:
-                    joint_right_hands[j] = [lm.x, lm.y, lm.z, setVisibility(lm.x, lm.y)]
-                
-            # 손 랜드마크 그리기
-            if handedness.classification[0].label == 'Left':
-                mp_drawing.draw_landmarks(frame, res, mp_hands.HAND_CONNECTIONS, landmark_drawing_spec=mp_drawing.DrawingSpec(color=(0, 255, 0)))   # green
-            else:
-                mp_drawing.draw_landmarks(frame, res, mp_hands.HAND_CONNECTIONS, landmark_drawing_spec=mp_drawing.DrawingSpec(color=(255, 0, 0)))   # blue
+                    mp_drawing.draw_landmarks(frame, res, mp_hands.HAND_CONNECTIONS, landmark_drawing_spec=mp_drawing.DrawingSpec(color=(255, 0, 0)))   # blue
+            
+        # 포즈 검출시
+        if results_pose.pose_landmarks is not None:
+            for j, i in enumerate(pose_landmark_indices):
+                plm = results_pose.pose_landmarks.landmark[i]
+                joint[j] = np.concatenate([joint_left_hands[j], joint_right_hands[j], [plm.x, plm.y, plm.z, plm.visibility]])
+            # 포즈 랜드마크 그리기
+            mp_drawing.draw_landmarks(frame, results_pose.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+
+        d = np.array(joint.flatten())
+        data.append(d)
+        print(data) # joint가 pose is not none일 때만 돌아서 상체 일부 포즈 랜드마크 추출 x-> data 배열의 hands 데이터가 다 0으로 채워짐
+
+        # 화면에 표시
+        cv2.imshow('MediaPipe', frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+        img_pil = Image.fromarray(frame)
+        draw = ImageDraw.Draw(img_pil)
+        draw.text((30,30), "수어", font=font, fill=(0,0,0))
+        frame = np.array(img_pil)
+
+    data = np.array(data)
+
+    full_seq_data = [data[seq:seq + seq_length] for seq in range(len(data) - seq_length)]
+    full_seq_data = np.array(full_seq_data)
+    print("seq data shape:", full_seq_data.shape)
     
-    # 포즈 검출시
-    if results_pose.pose_landmarks is not None:
-        # 전체 데이터(joint) 생성, 포즈 -> 지정한 관절에 대해서만 반복
-        for j, i in enumerate(pose_landmark_indices):
-            plm = results_pose.pose_landmarks.landmark[i]
-            joint[j] = np.concatenate([joint_left_hands[j], joint_right_hands[j], [plm.x, plm.y, plm.z, plm.visibility]])
+    # 예측
+    y_pred = model.predict(full_seq_data)
 
-        # 포즈 랜드마크 그리기
-        mp_drawing.draw_landmarks(frame, results_pose.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+    mean_pred = np.mean(np.array(y_pred), axis=0)
+    print("mean: ", mean_pred)
 
-    d = np.array(joint.flatten())
+    max_pred = int(np.argmax(mean_pred))
+    print("max_pred_idx: ", max_pred)
 
-    seq.append(d)
+    conf = mean_pred[max_pred]
+    print(f"conf: {conf:.3f}")
 
-    if len(seq) < seq_length:
-        continue
-
-    input_data = np.expand_dims(np.array(seq[-seq_length:], dtype=np.float32), axis=0)
-    
-    y_pred = model.predict(input_data).squeeze()
-
-    i_pred = int(np.argmax(y_pred))
-    conf = y_pred[i_pred]
-
-    if conf > 0.90 and results_hands.multi_hand_landmarks is not None:
-        action = actions[i_pred]
-        print(action)
-        action_seq.append(action)
-
-        if len(action_seq) < 5 :
-            continue
-
-        if action_seq[-1] == action_seq[-2] == action_seq[-3] == action_seq[-4] == action_seq[-5]:
-            img_pil = Image.fromarray(frame)
-            draw = ImageDraw.Draw(img_pil)
-            draw.text((40,40), action, font=font, fill=(255,255,255))
-            frame = np.array(img_pil)
-    else:
-        cv2.putText(frame, '-', org=(50,50), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
-
-    # 화면에 표시
-    cv2.imshow('MediaPipe', frame)
-    if cv2.waitKey(1) == ord('q'):
-        break
+    action = actions[max_pred]
+    print("예측결과: ", action)
 
 cap.release()
 cv2.destroyAllWindows()
