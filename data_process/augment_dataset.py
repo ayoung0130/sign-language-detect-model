4749508494f7fb711bd6c4d5791a57f03a898bcb
ayoung0130 <149597700+ayoung0130@users.noreply.github.com>
2024-05-07 19:08:08 +0900

    0507

--- data_process/augment_dataset.py
+++ data_process/augment_dataset.py
@@ -1,11 +1,11 @@
 import numpy as np
 import os
-folder_path = "C:/Users/_/Desktop/pad_npy_data"
-# folder_path = "C:/Users/_/Desktop/slice_npy_data"
+# folder_path = "C:/Users/mshof/Desktop/pad_npy_data"
+folder_path = "C:/Users/mshof/Desktop/slice_npy_data"
 # folder_path = "C:/Users/mshof/Desktop/shift_npy_data"
 
-save_path = "C:/Users/_/Desktop/flip_pad_npy_data"
-# save_path = "C:/Users/_/Desktop/flip_slice_npy_data"
+# save_path = "C:/Users/mshof/Desktop/flip_pad_npy_data"
+save_path = "C:/Users/mshof/Desktop/flip_slice_npy_data"
 # save_path = "C:/Users/mshof/Desktop/flip_shift_npy_data"
 
 def flip():
@@ -15,14 +15,17 @@ def flip():
         base_name = os.path.basename(file_path)
         data = np.load(file_path)
 
-        print("flip 전: ", data[500, 248:253])
+        print("flip 전: ", data[100, 248:253])
 
+        # 0번 인덱스부터 249번 인덱스까지
         # 1 - (x 좌표값)
-        for x in range(0, 249, 4):
-            data[:, x] = 1 - data[:, x]
+        for x in range(0, 250, 4):
+            nonzero_indices = np.nonzero(data[:, x])[0]  # 0이 아닌 값의 인덱스를 찾습니다.
+            if len(nonzero_indices) > 0:  # 0이 아닌 값이 하나라도 있는 경우에만 연산을 수행합니다.
+                data[:, x][nonzero_indices] = 1 - data[:, x][nonzero_indices]
 
         
-        print("flip 후: ", data[500, 248:253])
+        print("flip 후: ", data[100, 248:253])
         print("")
 
         # 수정된 데이터를 저장
