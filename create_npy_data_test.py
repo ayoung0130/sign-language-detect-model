8ff3ca6984d711fa9cfa0a0ca61436cf510e9d36
ayoung0130 <149597700+ayoung0130@users.noreply.github.com>
2024-07-07 22:03:43 +0900

    test 영상 numpy 변환, 모델 예측

---
+++ create_npy_data_test.py
@@ -0,0 +1,68 @@
+import cv2
+import numpy as np
+import os, time
+from landmark_processing import get_landmarks
+from dotenv import load_dotenv
+
+# 테스트 영상을 넘파이 배열로 변환하는 코드
+
+load_dotenv()
+base_dir = os.getenv('BASE_DIR')
+
+folder_path = os.path.join(base_dir, f"test_video/3_test")
+
+# 데이터 저장 경로
+save_path = os.path.join(base_dir, "test")
+flip_save_path = os.path.join(base_dir, "test_flip")
+
+video_num = 0
+
+for video_file in os.listdir(folder_path):
+    video_num += 1
+    # 동영상 불러오기
+    video_path = os.path.join(folder_path, video_file)
+    base_name = os.path.splitext(os.path.basename(video_path))[0]  # 확장자를 제거한 이름
+    cap = cv2.VideoCapture(video_path)
+
+    data = []
+    flip_data = []
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        
+        # 랜드마크, 프레임 가져오기
+        d, original_frame = get_landmarks(frame, True)
+    
+        # 전체 데이터 배열에 추가
+        data.append(d)
+
+        if "오른쪽" not in base_name and "왼쪽" not in base_name:
+            # 좌우반전된 프레임 처리
+            flipped_frame = cv2.flip(frame, 1)
+            d_flipped, flipped_frame = get_landmarks(flipped_frame, True)
+            flip_data.append(d_flipped)
+
+            # 화면에 표시
+            cv2.imshow('Flipped', flipped_frame)
+
+        # 화면에 표시
+        cv2.imshow('Original', original_frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    # 넘파이 배열로 생성
+    data = np.array(data)
+    flip_data = np.array(flip_data)
+    print("data shape: ", data.shape)
+
+    # 넘파이 데이터 저장
+    np.save(os.path.join(save_path, f'{base_name}'), data)
+    if flip_data.size > 0:  # flip_data가 비어있지 않은 경우에만 저장
+        np.save(os.path.join(flip_save_path, f'flip_{base_name}'), flip_data)
+    
+print("테스트 영상 넘파이 변환 완료")
+print("영상 개수: ", video_num)
+# 사용된 함수, 자원 해제
+cv2.destroyAllWindows()
---
+++ test_npy.py
@@ -0,0 +1,98 @@
+import os, random
+import numpy as np
+from setting import actions, seq_length
+from keras.models import load_model
+from landmark_processing import get_landmarks
+from collections import Counter
+from dotenv import load_dotenv
+
+# numpy 파일로 모델 예측을 수행하는 코드
+
+load_dotenv()
+base_dir = os.getenv('BASE_DIR')
+
+# 모델 불러오기
+model = load_model('models/model_50words_0630.h5')
+
+# 비디오 파일 설정
+npy_data = os.path.join(base_dir, 'test')
+
+# 동영상 파일 목록 랜덤으로 섞기
+npy_files = os.listdir(npy_data)
+random.shuffle(npy_files)
+npy_file_count = len(npy_files)
+flip_npy_file_count = len([file for file in npy_files if "flip" in file])
+
+correct_count = 0
+flip_correct_count = 0
+
+# 각 action별 정답 수를 저장할 딕셔너리 초기화
+action_correct_counts = {action: 0 for action in actions}
+data = []
+
+for npy_file in npy_files:
+    # 동영상 불러오기
+    file_path = os.path.join(npy_data, npy_file)
+    base_name = os.path.basename(file_path)
+
+    data = np.load(file_path)
+
+    full_seq_data = [data[seq:seq + seq_length] for seq in range(0, len(data) - seq_length + 1, 10)]
+    full_seq_data = np.array(full_seq_data)
+    print(full_seq_data.shape)
+
+    # 예측
+    y_pred = model.predict(full_seq_data)
+
+    # 각 프레임의 가장 높은 확률을 가지는 클래스 선택 (90% 이상일 때만)
+    predicted_classes = []
+    for frame_predictions in y_pred:
+        max_prob = np.max(frame_predictions)
+        if max_prob >= 0.90:
+            predicted_class = np.argmax(frame_predictions)
+            predicted_classes.append(predicted_class)
+
+    # predicted_class가 없다면(90% 이상인 경우가 없다면)
+    if not predicted_classes:
+        print("신뢰도가 낮습니다")
+        print("정답: ", base_name)
+    else:
+        print(predicted_classes)
+        # 다수결 투표 방식으로 최종 예측 결정
+        vote_counts = Counter(predicted_classes)
+        final_prediction, final_prediction_count = vote_counts.most_common(1)[0]
+
+        action = actions[final_prediction]
+
+        # 정답 출력/개수 계산
+        print("예측결과: ", action)
+        print("정답: ", base_name)
+        if action in base_name:
+            correct_count += 1
+            action_correct_counts[action] += 1
+            if "flip" in base_name:
+                flip_correct_count += 1
+
+        # # 예측값을 넘파이 파일로 저장
+        # save_path = os.path.join(base_dir, f"pred/{base_name}_{action}.npy")
+        # np.save(save_path, y_pred)
+
+print("")
+print("결과")
+
+# 각 action별 정답 확률 출력
+for action, correct in action_correct_counts.items():
+    word_count = 6
+    if "오른쪽" in action or "왼쪽" in action:
+        word_count = 4
+    accuracy = (correct / word_count) * 100
+    print(f"{action} --> {accuracy:.2f}% ({correct} / {word_count})")
+
+# 총 정답 개수
+print("\n총 정답 수:", correct_count, "/", npy_file_count)
+
+# flip 영상이 있다면
+if flip_npy_file_count > 0:
+    print("좌우반전 정답 수:", flip_correct_count, "/", flip_npy_file_count)
+
+print(f"정답 확률: {(correct_count / npy_file_count * 100):.2f}%")
