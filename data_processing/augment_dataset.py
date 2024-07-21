abf116e47779841e2bee0ed82c54232577d5d50c
ayoung0130 <149597700+ayoung0130@users.noreply.github.com>
2024-07-21 22:56:57 +0900

    포즈 각도값 수정 버전 npy

--- data_processing/augment_dataset.py
+++ data_processing/augment_dataset.py
@@ -8,12 +8,12 @@ load_dotenv()
 base_dir = os.getenv('BASE_DIR')
 
 # 6/16 - shift
-folder_path = os.path.join(base_dir, 'npy_angle')
+folder_path = os.path.join(base_dir, 'npy_angle_10_words')
 
-save_path = os.path.join(base_dir, 'npy_angle_shift')
+save_path = os.path.join(base_dir, 'npy_angle_shift_10_words')
 
 def augment_data():
-    scales = [0.9, 1.1]
+    scales = [0.8, 0.9, 1.1, 1.2]
 
     for npy_file in os.listdir(folder_path):
         # 파일 불러오기
